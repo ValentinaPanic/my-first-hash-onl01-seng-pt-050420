a5bf02fb331bb672032ff6fce0e25447aa5b436f
Valentina Panic <valentina.panic87@gmail.com>
2020-05-15 01:35:41 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,8 +24,6 @@ def retrieval
                   "oil paintings" => 3
                  }
 shipping_manifest["oil paintings"]
-shipping_manifest["muskets"]= 2
-shipping_manifest["gun powder"]=4
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
